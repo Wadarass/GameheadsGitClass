fd2252b3bac142032c656246545c82484d892ff1
Wadarass <raymond.graham4087@gmail.com>
2020-04-11 15:07:19 -0700

    Basic Move Script
    Added a basic Move script to show how transforms work.

---
+++ IntroUnitySummer2020/Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Speed is a Vector3 defines how fast to move in 3D space
+        gameObject.transform.Translate(speed);
+    }
+}
