2492c17f16120ab9c54cf6a641f7f89b8055a9e8
Wadarass <raymond.graham4087@gmail.com>
2020-04-18 14:56:26 -0700

    Week 4 Gameheads camera follow
    camera follow stuff.

---
+++ IntroUnitySummer2020/Assets/Scripts/Follow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject target;
+    public float followDistance;
+    public float cameraFollowSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 targetPosition = target.transform.position;
+
+        Vector3 followVector = transform.forward * -followDistance;
+
+        transform.position = targetPosition + followVector;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = target.transform.position;
+
+        Vector3 followVector = transform.forward * -followDistance;
+
+        Vector3 cameraTargetPosition = targetPosition + followVector;
+
+        transform.position = Vector3.Lerp(transform.position, cameraTargetPosition, 
+            cameraFollowSpeed * Time.deltaTime);
+    }
+}
--- IntroUnitySummer2020/Assets/Scripts/Move.cs
+++ IntroUnitySummer2020/Assets/Scripts/Move.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public Vector3 speed;
+    public Vector3 speed; //Amount of units to move per second
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +15,26 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 currentSpeed = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentSpeed.x = -speed.x; 
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentSpeed.x = speed.x;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentSpeed.z = speed.z;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentSpeed.z = -speed.z;
+        }
+
         //Speed is a Vector3 defines how fast to move in 3D space
-        gameObject.transform.Translate(speed);
+        gameObject.transform.Translate(currentSpeed * Time.deltaTime);
     }
 }
