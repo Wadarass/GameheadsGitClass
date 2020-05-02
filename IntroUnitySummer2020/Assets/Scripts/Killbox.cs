e5959a2d830d81b0f4c55e5afa0e27959f15ea82
Wadarass <raymond.graham4087@gmail.com>
2020-05-02 14:19:13 -0700

    mulitple jump and Physics kill box

---
+++ IntroUnitySummer2020/Assets/Scripts/Killbox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killbox : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("CapsulePlayer") )
+        {
+            Debug.Log("Player hit me!");
+
+            other.gameObject.transform.position = spawnPoint.position;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
--- IntroUnitySummer2020/Assets/Scripts/Move.cs
+++ IntroUnitySummer2020/Assets/Scripts/Move.cs
@@ -8,24 +8,23 @@ public class Move : MonoBehaviour
     //Amount of degrees per second to turn
     public float turnSpeed;
     public float jumpForce = 10.0f;
-
+    
     private bool isJumping = false;
+    private float currentSpeed = 0.0f;
+    private float distanceToGround = 0.0f;
+    private int jumpCount = 0;
     private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        distanceToGround = GetComponent<Collider>().bounds.extents.y;
     }
 
     void Update()
     {
-        
-    }
-
-    void FixedUpdate()
-    {
-        float currentSpeed = 0.0f;
+        currentSpeed = 0.0f;
         float currentTurnAmount = 0.0f;
 
         if (Input.GetKey(KeyCode.A))
@@ -45,14 +44,31 @@ public class Move : MonoBehaviour
             currentSpeed = -speed.x;
         }
 
-        //Speed is a Vector3 defines how fast to move in 3D space
         gameObject.transform.Rotate(Vector3.up, currentTurnAmount * Time.deltaTime);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, distanceToGround + 0.1f);
+//        return (Mathf.Abs(rb.velocity.y) < Mathf.Epsilon);
+    }
+
+    void FixedUpdate()
+    {
+        //Speed is a Vector3 defines how fast to move in 3D space
         rb.AddForce(transform.forward * currentSpeed * Time.deltaTime, ForceMode.Impulse);
 
-        if (Input.GetKeyUp(KeyCode.Space) && !isJumping)
+        bool isGrounded = IsGrounded();
+
+        if (isGrounded)
         {
-            isJumping = true;
+            jumpCount = 0;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && (isGrounded || jumpCount < 2))
+        {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpCount = jumpCount + 1;
         }
 
         rb.angularVelocity = Vector3.zero;
