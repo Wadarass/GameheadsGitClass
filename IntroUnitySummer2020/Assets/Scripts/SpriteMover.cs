08ad023ce0316b874d482f42dbe5473693fb70b0
Wadarass <raymond.graham4087@gmail.com>
2020-05-09 15:14:46 -0700

    2D class checkin

---
+++ IntroUnitySummer2020/Assets/Scripts/SpriteMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMover : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float currentSpeed = 0.0f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentSpeed -= speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentSpeed += speed;
+        }
+
+        rb.AddForce(new Vector2(currentSpeed * Time.deltaTime, 0.0f), ForceMode2D.Impulse);
+    }
+}
