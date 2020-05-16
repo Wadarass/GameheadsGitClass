ab6fd8f0615c5f7d92b37a6f0fc679888eb97193
Wadarass <raymond.graham4087@gmail.com>
2020-05-16 13:45:25 -0700

    Updateed sprite mover

--- IntroUnitySummer2020/Assets/Scripts/SpriteMover.cs
+++ IntroUnitySummer2020/Assets/Scripts/SpriteMover.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class SpriteMover : MonoBehaviour
 {
     public float speed = 5;
+    public float jumpSpeed = 2.0f;
+
     private Rigidbody2D rb;
+    private float distanceToGround = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        distanceToGround = GetComponent<Collider2D>().bounds.extents.y;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    bool IsGrounded()
+    {
+        return Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.down, distanceToGround + 0.1f);
+        // return (Mathf.Abs(rb.velocity.y) < Mathf.Epsilon)
+    }
+
+// Update is called once per frame
+void FixedUpdate()
     {
         float currentSpeed = 0.0f;
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -26,5 +37,10 @@ public class SpriteMover : MonoBehaviour
         }
 
         rb.AddForce(new Vector2(currentSpeed * Time.deltaTime, 0.0f), ForceMode2D.Impulse);
+
+        if (Input.GetKeyUp(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        }
     }
 }
