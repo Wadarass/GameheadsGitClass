73046e705575e95c5570de371c0059a78d338fb1
Wadarass <raymond.graham4087@gmail.com>
2020-05-16 16:33:53 -0700

    Comments
    Adding some clarifying comments and cleaned up a bit.

--- IntroUnitySummer2020/Assets/Scripts/SpriteMover.cs
+++ IntroUnitySummer2020/Assets/Scripts/SpriteMover.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SpriteMover : MonoBehaviour
 {
+    //Variables exposed to Unity Inspector
     public float speed = 5;
     public float jumpSpeed = 2.0f;
 
+    //Local variables that I don't expose to the Unity inspector
     private Rigidbody2D rb;
     private float distanceToGround = 0.0f;
     private bool isJumping = false;
@@ -28,14 +31,17 @@ public class SpriteMover : MonoBehaviour
 
     bool IsGrounded()
     {
-        
+        //Cast a ray from the bottom of the player collider to 0.1 units to find a collider.  
+        //Searching in every layer but the layer that belongs to the player
         RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - distanceToGround), Vector2.down, 0.1f, mask);
 
+        //If the collider isn't null that means we hit something.  We are grounded.
         return (hit.collider != null);
     }
 
     private void Update()
     {
+        //If I hit space key and I'm grounded I'm jumping
         if(Input.GetKeyUp(KeyCode.Space) && IsGrounded())
         {
             isJumping = true;
@@ -57,10 +63,11 @@ public class SpriteMover : MonoBehaviour
 
         rb.AddForce(new Vector2(currentSpeed * Time.deltaTime, 0.0f), ForceMode2D.Impulse);
 
+        //If I'm jumping add the force.
         if (isJumping)
         {
             rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
-            isJumping = false;
+            isJumping = false;   //Immediately stop jumping
         }
     }
 }
