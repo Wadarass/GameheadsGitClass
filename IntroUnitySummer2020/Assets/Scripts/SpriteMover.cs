5dedd9f265657f7274e1a54d237f513342523b69
Wadarass <raymond.graham4087@gmail.com>
2020-05-16 16:06:37 -0700

    TextMesh Pro and tilemap updates.

--- IntroUnitySummer2020/Assets/Scripts/SpriteMover.cs
+++ IntroUnitySummer2020/Assets/Scripts/SpriteMover.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpriteMover : MonoBehaviour
 {
@@ -9,22 +10,40 @@ public class SpriteMover : MonoBehaviour
 
     private Rigidbody2D rb;
     private float distanceToGround = 0.0f;
+    private bool isJumping = false;
+    private LayerMask mask;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Get the Player layer and then change the mask to every layer but the player
+        int maskValue = LayerMask.GetMask("Player");
+        mask = ~maskValue;
+
+        //Initialize the private variables to known values
+        isJumping = false;
         rb = GetComponent<Rigidbody2D>();
-        distanceToGround = GetComponent<Collider2D>().bounds.extents.y;
+        distanceToGround = GetComponent<CircleCollider2D>().radius + Mathf.Epsilon;
     }
 
     bool IsGrounded()
     {
-        return Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.down, distanceToGround + 0.1f);
-        // return (Mathf.Abs(rb.velocity.y) < Mathf.Epsilon)
+        
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - distanceToGround), Vector2.down, 0.1f, mask);
+
+        return (hit.collider != null);
     }
 
-// Update is called once per frame
-void FixedUpdate()
+    private void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space) && IsGrounded())
+        {
+            isJumping = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
         float currentSpeed = 0.0f;
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -38,9 +57,10 @@ void FixedUpdate()
 
         rb.AddForce(new Vector2(currentSpeed * Time.deltaTime, 0.0f), ForceMode2D.Impulse);
 
-        if (Input.GetKeyUp(KeyCode.Space) && IsGrounded())
+        if (isJumping)
         {
             rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            isJumping = false;
         }
     }
 }
